16d66e61fe9af05c041ef4cb600c561c9b32ef1f
Marko Vladisavljević <markovl007@gmail.com>
2024-02-17 15:49:56 +0100

    Final commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,44 +7,54 @@ function Header({ onDataUpdate }) {
   const [data, setData] = useState("");
   const [searchValue, setSearchValue] = useState("");
 
-
-    const fetchData = async () => {
-      console.log(searchValue);
-      try {
-        const apiKey = "bdc081bef27165738f86c22fa44f1ab3";
-
-        if (!searchValue) {
-          console.error("City is empty");
-          return;
-        }
-
-        const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${apiKey}`
-        );
-
-        console.log("API Response:", response);
-        setData(response.data);
-
-        onDataUpdate(response.data);
-      } catch (error) {
-        console.error("API Request Failed:", error);
-
-        if (error.response) {
-          console.error("Error Response Data:", error.response.data);
-        }
-      }
-    };
-
+  const fetchData = async () => {
     console.log(searchValue);
+    try {
+      const apiKey = "bdc081bef27165738f86c22fa44f1ab3";
+
+      if (!searchValue) {
+        console.error("City is empty");
+        return;
+      }
+
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${apiKey}`
+      );
+
+      console.log("API Response:", response);
+      setData(response.data);
+
+      onDataUpdate(response.data);
+    } catch (error) {
+      console.error("API Request Failed:", error);
+
+      if (error.response) {
+        console.error("Error Response Data:", error.response.data);
+      }
+    }
+  };
+
+  console.log(searchValue);
 
   return (
     <div className="header">
       <Search onSearchValueChange={setSearchValue} />
       <button onClick={() => fetchData()} type="button">
-        <img
-          src="https://imagedelivery.net/5MYSbk45M80qAwecrlKzdQ/91f34cc8-d5b1-4d66-4d1b-65b1a5205a00/preview"
-          alt=""
-        />
+        <svg
+          width="30px"
+          height="30px"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z"
+            stroke="#000000"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
       </button>
     </div>
   );
