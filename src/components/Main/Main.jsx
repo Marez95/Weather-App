6e28cd04816e40e7b7fa18381b8edb135bd91417
Marko Vladisavljević <markovl007@gmail.com>
2024-03-09 17:02:07 +0100

    Styling

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,21 +3,21 @@ import "./Main.css"
 function Main({ data }) {
     return (
       <div className="main">
-        <p>Temperature: {data?.main && data?.main?.temp} °F</p>
-        <p>Maximum temperature: {data?.main && data?.main?.temp_max} °F</p>
-        <p>Minimum temperature: {data?.main && data?.main?.temp_min} °F</p>
-        <p>Feels like: {data?.main && data?.main?.feels_like} °F</p>
-        <p>Humidity: {data?.main && data?.main?.humidity} g/kg</p>
-        <p>City: {data?.name}</p>
-        <p>Country: {data?.sys && data?.sys?.country}</p>
+        <p><u>Temperature:</u> {data?.main && data?.main?.temp} °F</p>
+        <p><u>Maximum temperature:</u> {data?.main && data?.main?.temp_max} °F</p>
+        <p><u>Minimum temperature:</u> {data?.main && data?.main?.temp_min} °F</p>
+        <p><u>Feels like:</u> {data?.main && data?.main?.feels_like} °F</p>
+        <p><u>Humidity:</u> {data?.main && data?.main?.humidity} g/kg</p>
+        <p><u>City:</u> {data?.name}</p>
+        <p><u>Country:</u> {data?.sys && data?.sys?.country}</p>
         <p>
-          Weather description: {data?.weather && data?.weather[0]?.main} (
+          <u>Country:</u> {data?.weather && data?.weather[0]?.main} (
           {data?.weather && data?.weather[0]?.description})
         </p>
-        <p>Wind speed: {data?.wind && data?.wind?.speed} km/h</p>
-        <p>Coordinates:</p>
-        <p>Latitude: {data?.coord && data?.coord?.lat}</p>
-        <p>Longitude: {data?.coord &&  data?.coord?.lon}</p>
+        <p><u>Wind speed:</u> {data?.wind && data?.wind?.speed} km/h</p>
+        <p><u>Coordinates:</u></p>
+        <p><u>Latitude:</u> {data?.coord && data?.coord?.lat}</p>
+        <p><u>Longitude:</u> {data?.coord &&  data?.coord?.lon}</p>
       </div>
     )
 }
